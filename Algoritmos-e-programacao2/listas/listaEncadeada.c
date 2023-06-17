c73c6c8d4a74b249bc094ad0b7b6937086e73194
Ângelo Vinicius Hernandez Mazarin <angelotrab22@gmail.com>
2023-06-17 14:54:54 -0400

    a

--- Algoritmos-e-programacao2/listas/listaEncadeada.c
+++ Algoritmos-e-programacao2/listas/listaEncadeada.c
@@ -5,17 +5,16 @@ struct no{
     struct no *prox;
 };
 typedef struct no No;
-struct lista
-{
+struct lista{
     No *inicio;
     int n;
 };
 typedef struct lista Lista;
-*Lista iniciaLista(){
+Lista iniciarLista(){
     Lista *lista = (Lista*)malloc(sizeof(Lista));
     lista->inicio = NULL;
     lista->n = 0;
-    return lista;
+    return *lista;
 }
 void inserirInicio(Lista* lista, int newInfo){
     No* novo = (No*)malloc(sizeof(No));
@@ -39,5 +38,19 @@ void busca(Lista *lista, int info){
         aux = aux->prox;
     }
     return NULL;
+}
+void remove(Lista*lista, int trash){
+    No *aux = lista->inicio; // cria ponteiro auxiliar para percorrer a lista
+    No *ant = NULL; //ponteiro auxiliar para obter o elemento anterior 
+    while (aux->prox != NULL){  //while com condição de parada no fim da lista
+        if(aux->info == trash)break; //condição de parada quando acha o elemento a ser removido
+        ant = aux; // anterior recebe o valor atual
+        aux = aux->prox; // atual recebe o proximo
+    }
+    if (aux==NULL)return; // verifica se o elemento foi encontrado
+    if(ant==NULL) lista->inicio = aux->prox; //verifica se o elemento está no inicio da lista e torna o proximo elemento o inicio caso seja verdadeiro
+    else ant->prox = aux->prox; // liga o elemento anterior ao proximo do atual para desencadea-lo
+    free(aux); // desencadea o elemento atual
+    lista->n--; // subtrai em 1 o tamanho da lista
 }
 int main(){}
